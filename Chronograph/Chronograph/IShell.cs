fbe311e5275576e1fc350cdf39e544a54162041d
Sarah Plotkin <pushasha@gmail.com>
2020-05-24 12:05:18 -0400

    Wipe project in preparation to start over in Xamarin Forms.

--- Chronograph/Chronograph/IShell.cs
+++
@@ -1,3 +0,0 @@
-namespace Chronograph {
-    public interface IShell {}
-}
--- Chronograph/Chronograph/ShellViewModel.cs
+++
@@ -1,3 +0,0 @@
-namespace Chronograph {
-    public class ShellViewModel : Caliburn.Micro.PropertyChangedBase, IShell { }
-}
